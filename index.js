3107ccffa156e31553e0b7ed40eb7ab8e274bf1e
MykalB <mykalb081@gmail.com>
2021-06-12 16:39:16 -0400

    Additional Setup
    - Remove server/server.js
    - Add an HTML view
    - Add CSS styling
    - Add page icon
    - Updage package.json
    - Create route to get timestamp

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 var express = require('express');
-let app = express()
+var favicon = require('serve-favicon');
 
+const app = express()
+
+// Set port.
+const PORT = process.env.PORT || 3001;
+
+console.log(__dirname + '/public');
+
+// Listen to port.
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
+// Middleware.
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/public', favicon(__dirname + '/public/favicon.png'));
+
+// Routes.
 app.get('/', (req, res) => {
     absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
-  });
+});
+
+app.get('/api/:time', (req, res) => {
+
+})
